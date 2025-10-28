83cc4e489e834edb04485fdd18ae6f1a45c6a137
Aditya Yadav <heyaadi2@gmail.com>
2025-10-28 10:26:01 +0530

    Update Gemini.js

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -7,7 +7,7 @@ import {
 
 
 
-const MODEL_NAME = "gemini-1.5-flash";
+const MODEL_NAME = "gemini-2.5-flash";
 const API_KEY = import.meta.env.VITE_GEMINI_API;
 
 async function runChat(prompt, history = []) {
